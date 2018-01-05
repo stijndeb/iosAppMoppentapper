6f46a5e29ae2d5b15814d88a0a0f2ba6914ffde4
stijndeb <stijn.debacker.w2871@student.hogent.be>
2018-01-05 03:36:20 +0100

    beoordeling werkt al deels

--- Moppentapper/Moppentapper/ViewControllers/PostDetailViewController.swift
+++ Moppentapper/Moppentapper/ViewControllers/PostDetailViewController.swift
@@ -1,13 +1,13 @@
-
-
+import Cosmos
 import UIKit
 
 class PostDetailViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
 
-    @IBOutlet weak var commnets: UICollectionView!
-    
-    @IBOutlet weak var postHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var PostCollectionView: UICollectionView!
+    @IBOutlet weak var postHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var comments: UICollectionView!
+    @IBOutlet weak var rating: CosmosView!
+    
     var post: Post!
     
     override func viewDidLoad() {
@@ -19,7 +19,7 @@ class PostDetailViewController: UIViewController, UICollectionViewDataSource, UI
             if let post = $0 {
                 self.post = post
                 self.PostCollectionView.reloadData()
-                self.commnets.reloadData()
+                self.comments.reloadData()
             }
         }
         PostCollectionView.register(UINib.init(nibName: "PostCell", bundle: nil), forCellWithReuseIdentifier: "PostCell")
@@ -28,12 +28,13 @@ class PostDetailViewController: UIViewController, UICollectionViewDataSource, UI
         }
         PostCollectionView.dataSource = self
         
-        commnets.register(UINib.init(nibName: "CommentCell", bundle: nil), forCellWithReuseIdentifier: "CommentCell")
-        if let flowLayout = commnets.collectionViewLayout as? UICollectionViewFlowLayout {
+        comments.register(UINib.init(nibName: "CommentCell", bundle: nil), forCellWithReuseIdentifier: "CommentCell")
+        if let flowLayout = comments.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.estimatedItemSize = CGSize(width: 1,height: 1)
         }
-        commnets.dataSource = self
+        comments.dataSource = self
         
+        rating.rating = Double(post.score())
         
         let size = CGSize(width: 400, height: 1000)
         
@@ -58,14 +59,17 @@ class PostDetailViewController: UIViewController, UICollectionViewDataSource, UI
         // Dispose of any resources that can be recreated.
     }
     
-   
+    @IBAction func Rate(_ sender: Any) {
+        print(rating.rating)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-        if collectionView == self.commnets {return post.comments.count}
+        if collectionView == self.comments {return post.comments.count}
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == self.commnets {
+        if collectionView == self.comments {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommentCell", for: indexPath) as! CommentCell
         
